468f6caa6ba0a627dc58c031821dc36a03a62daa
Leonardo <leonardocolodette0@gmail.com>
2015-09-28 10:12:29 -0300

    a007

--- Projeto0002/src/br/example/projeto001/RepositorioPessoa.java
+++ Projeto0002/src/br/example/projeto001/RepositorioPessoa.java
@@ -3,49 +3,11 @@ package br.example.projeto001;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RepositorioPessoa implements IRepositorioPessoa {
+public class RepositorioPessoa{
 
 	public static  List<Pessoa> listaPessoa = new ArrayList<Pessoa>();
 
-	public boolean inserirPessoa(Pessoa pessoa) {
 
-		try {
-
-			listaPessoa.add(pessoa);
-
-		} catch (Exception e) {
-
-			return false;
-
-		}
-
-		return true;
-	}
-
-	public boolean deletePessoa(Pessoa pessoa) {
-
-		try {
-
-			listaPessoa.remove(pessoa);
-
-		} catch (Exception e) {
-
-			return false;
-
-		}
-
-		return true;
-	}
-
-	public boolean alterarPessoa(Pessoa pessoa) {
-
-		return false;
-	}
-
-	public List<Pessoa> mostrarPessoa() {
-
-		return listaPessoa;
-	}
-
+	
 
 }
