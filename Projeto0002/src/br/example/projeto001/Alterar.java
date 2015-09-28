5ea54a5f03da16cb6e0e03a3c3d0350e94f7c444
Leonardo <leonardocolodette0@gmail.com>
2015-09-28 08:46:21 -0300

    a005

--- Projeto0002/src/br/example/projeto001/Alterar.java
+++ Projeto0002/src/br/example/projeto001/Alterar.java
@@ -2,14 +2,19 @@ package br.example.projeto001;
 
 
 import android.app.Activity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class Alterar extends Activity{
 		
-	int i;
+		int i;
 		Spinner spinner;
+		EditText edNome,edIdade,edCpf;
 		
 		@Override
 		protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +22,11 @@ public class Alterar extends Activity{
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.alterar);
 			
+			
 			spinner = (Spinner) findViewById(R.id.spinnerAlterar);
+			edNome = (EditText) findViewById(R.id.iptNome);
+			edIdade = (EditText) findViewById(R.id.iptIdade);
+			edCpf = (EditText) findViewById(R.id.iptCpf);
 			
 			String[] itens = new String[RepositorioPessoa.listaPessoa.size()];
 			
@@ -30,7 +39,31 @@ public class Alterar extends Activity{
 			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 			spinner.setAdapter(adapter);
 			
-			/*more code*/
 			
+			/*more code*/
+			spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+				@Override
+				public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+					
+					
+					edNome.setText(RepositorioPessoa.listaPessoa.get(spinner.getSelectedItemPosition()).getNome());
+					edIdade.setText(String.valueOf(RepositorioPessoa.listaPessoa.get(spinner.getSelectedItemPosition()).getIdade()));
+					edCpf.setText(String.valueOf(RepositorioPessoa.listaPessoa.get(spinner.getSelectedItemPosition()).getCpf()));
+					
+				
+				}
+
+				@Override
+				public void onNothingSelected(AdapterView<?> parent) {
+					
+					
+				}
+		
+			});
+			
+		
+		
+		
 		}
 }
