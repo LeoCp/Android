cfd5fb8598f31d36054ccfdd35b056d759e0a262
Leonardo <leonardocolodette0@gmail.com>
2015-09-28 09:19:43 -0300

    a005

--- Projeto0002/src/br/example/projeto001/Alterar.java
+++ Projeto0002/src/br/example/projeto001/Alterar.java
@@ -2,19 +2,23 @@ package br.example.projeto001;
 
 
 import android.app.Activity;
-
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-public class Alterar extends Activity{
+public class Alterar extends Activity implements OnClickListener{
 		
 		int i;
 		Spinner spinner;
 		EditText edNome,edIdade,edCpf;
+		Button butAlterar,butVoltarAlt;
 		
 		@Override
 		protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,12 @@ public class Alterar extends Activity{
 			edNome = (EditText) findViewById(R.id.iptNome);
 			edIdade = (EditText) findViewById(R.id.iptIdade);
 			edCpf = (EditText) findViewById(R.id.iptCpf);
+			
+			butAlterar = (Button) findViewById(R.id.butAlterar);
+			butAlterar.setOnClickListener(this);
+			
+			butVoltarAlt = (Button) findViewById(R.id.butVoltarAlterar);
+			butVoltarAlt.setOnClickListener(this);
 			
 			String[] itens = new String[RepositorioPessoa.listaPessoa.size()];
 			
@@ -65,5 +75,36 @@ public class Alterar extends Activity{
 		
 		
 		
+		}
+
+		@Override
+		public void onClick(View v) {
+			
+			switch (v.getId()) {
+			case R.id.butAlterar:
+				
+			RepositorioPessoa.listaPessoa.get(spinner.getSelectedItemPosition()).setNome(String.valueOf(edNome.getText().toString()));
+			RepositorioPessoa.listaPessoa.get(spinner.getSelectedItemPosition()).setIdade(Integer.valueOf(edIdade.getText().toString()));
+			RepositorioPessoa.listaPessoa.get(spinner.getSelectedItemPosition()).setCpf(Integer.valueOf(edCpf.getText().toString()));
+			
+			AlertDialog.Builder y = new AlertDialog.Builder(this);
+			y.setMessage("Alterações feitas com sucesso!");
+			y.setNeutralButton("Ok", null);
+			y.show();
+			
+			
+			break;
+			
+			case R.id.butVoltarAlterar:
+			
+			Intent intent = new Intent(this, Principal.class);
+			startActivity(intent);
+				
+			break;
+
+			default:
+				break;
+			}
+			
 		}
 }
