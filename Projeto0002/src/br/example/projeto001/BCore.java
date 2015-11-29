ea2b949c1a035473d7ef1a208da880b5e25b09ff
Leonardo <leonardocolodette0@gmail.com>
2015-11-29 11:20:52 -0200

    bdCore-don

--- Projeto0002/src/br/example/projeto001/BCore.java
+++ Projeto0002/src/br/example/projeto001/BCore.java
@@ -1,5 +1,30 @@
 package br.example.projeto001;
 
-public class BCore {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
+public class BCore extends SQLiteOpenHelper{
+	private static final String BANCO = "bdProj";
+	private static final int VERSAO = 1;
+	
+	public BCore(Context c) {
+		super(c,BANCO,null,VERSAO);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("create table pessoa(id integer primary key autoincrement,"
+				+ "nome text"
+				+ "idade text"
+				+ "cpf text );");
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL("drop table if exists pessoa");
+		onCreate(db);
+	}
+
+	
 }
